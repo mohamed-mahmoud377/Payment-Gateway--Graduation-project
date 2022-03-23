28facffa50204c115dedac10dc40cb0c45db3425
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-03-23 02:53:27 +0200

    project starter

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,9 @@ app.set('trust proxy',true); // to make sure that you trust nginx ingress proxy
 app.get('/api/users/jerry', (req:Request, res:Response)=>{
     res.send('hello mo2men & walid and dabos and sara and eman from the cloud <3 ')
 })
+app.get('/api/users/zeyed', (req:Request, res:Response)=>{
+    res.send('zeyed 7abeb 2lbe')
+})
 
 app.listen(3000,()=>{
     console.log("listening on port 4444....    ")
