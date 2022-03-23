f49daba7da2450ce52340454a5560141db512a50
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-03-23 02:42:50 +0200

    project starter

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ import 'express-async-errors'
 const app  = express();
 app.set('trust proxy',true); // to make sure that you trust nginx ingress proxy
 app.get('/api/users/jerry', (req:Request, res:Response)=>{
-    res.send('hello feysl')
+    res.send('hello mo2men & walid and dabos and sara and eman from the cloud <3 ')
 })
 
 app.listen(3000,()=>{
