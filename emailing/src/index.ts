c1d0dd8848c1d27b0e72a397435bf68f54167d19
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-03-28 21:37:54 +0200

    emailing srv  fix

--- emailing/src/index.ts
+++ emailing/src/index.ts
@@ -16,8 +16,6 @@ const startUp = async ()=>{
     if (!process.env.NATS_CLUSTER_ID){
         throw new Error("NATS_CLUSTER_ID must be defined !")
     }
-
-
     try{
         await natsWrapper.connect(process.env.NATS_CLUSTER_ID,process.env.NATS_CLIENT_ID,process.env.NATS_URL)
         natsWrapper.client.on('close',()=>{
