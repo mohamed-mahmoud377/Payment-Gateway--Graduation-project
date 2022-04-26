96a0b3856cb054760a182868e5e9cb540a00e022
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-04-27 00:01:16 +0200

    adding delete token route

--- kms/src/app.ts
+++ kms/src/app.ts
@@ -14,6 +14,7 @@ import {getServiceAccessToken} from "./routes/getServiceAccessToken";
 import {getTokens} from "./routes/getTokens";
 import {dataEncryptKey} from "./routes/dataEncryptKey";
 import {decryptDataEncryptKey} from "./routes/decryptDataEncryptKey";
+import {deleteToken} from "./routes/deleteToken";
 
 
 const app= express();
@@ -38,6 +39,7 @@ app.use(cookieSession({
     secure: false// note that even in production you will have to disable it
 }))
 
+app.use('/api/kms',deleteToken)
 app.use('/api/kms',decryptDataEncryptKey);
 app.use('/api/kms',dataEncryptKey)
 app.use('/api/kms',loginRoute)
---
+++ kms/src/routes/deleteToken.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response} from "express";
+import {NotFoundError, requireAuth, sendSuccess, validateRequest} from "@hashcash/common";
+import {jwtGenerator} from "../utils/jwtGenerator";
+
+import {Token} from "../models/tokens";
+import mongoose from "mongoose";
+
+
+const router = express.Router();
+
+
+router.delete('/tokens/:id',
+    requireAuth,async (req:Request,res:Response)=>{
+        const {id} = req.query;
+        if (!mongoose.isValidObjectId(id)){
+            throw new NotFoundError(['token not found']);
+        }
+        const token =await  Token.findByIdAndDelete(id);
+        if (!token){
+            throw new NotFoundError(['token not found']);
+        }
+
+        sendSuccess(res,204);
+
+    })
+
+export {router as deleteToken}
