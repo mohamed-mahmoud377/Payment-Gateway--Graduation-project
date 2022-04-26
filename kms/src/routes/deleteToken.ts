8053b0663b7efd3eb4733915c1d4f1fa6ca3e480
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-04-27 00:06:19 +0200

    adding delete token route fix

--- kms/src/routes/deleteToken.ts
+++ kms/src/routes/deleteToken.ts
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.delete('/tokens/:id',
     requireAuth,async (req:Request,res:Response)=>{
-        const {id} = req.query;
+        const {id} = req.params;
         if (!mongoose.isValidObjectId(id)){
             throw new NotFoundError(['token not found']);
         }
