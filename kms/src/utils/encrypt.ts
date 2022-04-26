5261e15c99bd12b2de91accfb36dd7036f9cc24d
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-04-26 07:12:28 +0200

    addin data encrypt key route in kms and updating genereate master create protect middleware fix

--- kms/src/utils/encrypt.ts
+++ kms/src/utils/encrypt.ts
@@ -1,8 +1,11 @@
 import {createCipheriv} from "crypto";
 
 export const encrypt = (data:any, key:string) => {
+    console.log(key)
          const [actualKey,iv] = key.split('.');
-    const cipher = createCipheriv('aes256',actualKey,iv);
+    console.log(actualKey , iv);
+         const ivBuffer = Buffer.from(iv,"hex")
+    const cipher = createCipheriv('aes256',actualKey,ivBuffer);
 
     return cipher.update(JSON.stringify(data),'utf8','hex')+cipher.final('hex')
 }
