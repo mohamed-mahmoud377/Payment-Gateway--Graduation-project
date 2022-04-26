80f676752bfd4a22f42bab1c8bc78a077c2d474e
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-04-26 18:13:27 +0200

    adding decypt encrypted key update

--- kms/src/utils/encrypt.ts
+++ kms/src/utils/encrypt.ts
@@ -7,8 +7,8 @@ export const encrypt = (data: string |any, key:string) => {
          const ivBuffer = Buffer.from(iv,"hex")
         const keyBuffer = Buffer.from(actualKey,"hex")
     const cipher = createCipheriv('aes256',keyBuffer,ivBuffer);
-        if (!(data instanceof String))
-            data = JSON.stringify(data);
+        // if (!(data instanceof String))
+        //     data = JSON.stringify(data);
 
     return cipher.update(data,'utf8','hex')+cipher.final('hex')
 }
