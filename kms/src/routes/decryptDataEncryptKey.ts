fd27ff166c00d2520836fada3272a076fee7707c
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-04-26 17:51:54 +0200

    adding decypt encrypted key fix

--- kms/src/routes/decryptDataEncryptKey.ts
+++ kms/src/routes/decryptDataEncryptKey.ts
@@ -4,7 +4,7 @@ import {generateKey} from "../utils/generateKey";
 import crypto from "crypto";
 import * as assert from "assert";
 import {Key} from "../models/key";
-import {NotFoundError, sendSuccess} from "@hashcash/common";
+import {BadRequestError, NotFoundError, sendSuccess} from "@hashcash/common";
 import {encrypt} from "../utils/encrypt";
 import {body} from "express-validator";
 import {decrypt} from "../utils/decrypt";
@@ -18,22 +18,27 @@ router.post('/decrypt-key',[
         .notEmpty()
         .withMessage('encrypted data encryption key must be provided to decrypt')
 ],protect,(async (req:Request, res:Response) =>{
-    const {encryptedKey} = req.body;
+    try{
+        const {encryptedKey} = req.body;
 
-    //get the master key for this token
-    const hashedMasterId =  crypto.createHash('sha256').update(req.payload?.masterId!).digest('hex');
+        //get the master key for this token
+        const hashedMasterId =  crypto.createHash('sha256').update(req.payload?.masterId!).digest('hex');
 
-    const masterKey = await Key.findOne({hashedId:hashedMasterId});
-    if (!masterKey){
-        throw new NotFoundError(['There not masterKey associated with you token'])
+        const masterKey = await Key.findOne({hashedId:hashedMasterId});
+        if (!masterKey){
+            throw new NotFoundError(['There not masterKey associated with you token'])
+        }
+
+        //encrypt the dataEncryption key with the master-key
+        const decryptedDataEncryptKey = decrypt(encryptedKey,masterKey.key);
+
+        // send them both plaint text and encrypted dataEncryption key\
+
+        sendSuccess(res,201,{dataEncryptKey:decryptedDataEncryptKey});
+
+    }catch (e) {
+        throw  new BadRequestError(['unable to decrypt your key'])
     }
-
-    //encrypt the dataEncryption key with the master-key
-    const decryptedDataEncryptKey = decrypt(encryptedKey,masterKey.key);
-
-    // send them both plaint text and encrypted dataEncryption key\
-
-    sendSuccess(res,201,{dataEncryptKey:decryptedDataEncryptKey});
 
 } ))
 
--- kms/src/utils/decrypt.ts
+++ kms/src/utils/decrypt.ts
@@ -2,6 +2,8 @@ import {createDecipheriv} from "crypto";
 
 export const decrypt = (encryptedMessage:string,key:string) => {
     const [actualKey,iv] = key.split('.');
-    const decipher = createDecipheriv('aes256',actualKey,iv);
+    const ivBuffer = Buffer.from(iv,"hex")
+    const keyBuffer = Buffer.from(actualKey,"hex")
+    const decipher = createDecipheriv('aes256',keyBuffer,ivBuffer);
     return decipher.update(encryptedMessage, 'hex', 'utf8') + decipher.final('utf8');
 }
