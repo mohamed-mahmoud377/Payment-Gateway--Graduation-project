44d0c62e738f98e421018799922371e17c67c0cc
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-06-01 22:13:47 +0200

    everything needed to run apikey-manager serivce

---
+++ apikey-manager/src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import 'express-async-errors'
+// import PinoHttp from "express-pino-logger";
+import bodyParser from 'body-parser'
+import cookieSession from "cookie-session";
+
+import {errorHandler, secure} from "@hashcash/common";
+
+
+
+const app= express();
+
+app.set('trust proxy',true);
+
+app.use(bodyParser.json());
+
+
+secure(app,{
+    bodyLimiting: true,
+    compression: true,
+    cors: true,
+    helmet: true,
+    noSQLInjectionSanitization: true,
+    rateLimiter: true,
+    xss: true
+})
+
+app.use(cookieSession({
+    sameSite:"lax",
+    signed:false,
+    httpOnly:true, // to prevent xss and means it can never be accessed by JS
+    secure: false// note that even in production you will have to disable it because we are not https in prod yet
+}))
+
+
+
+
+
+app.use(errorHandler)
+
+ export {app}
