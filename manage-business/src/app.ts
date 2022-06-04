63faaa9abf10814c0192dc886b44063b9354a34f
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-06-04 21:46:26 +0200

    getting everything to work

--- manage-business/src/app.ts
+++ manage-business/src/app.ts
@@ -9,6 +9,7 @@ import {getAllActivationRequestsRoute} from "./routes/AllActivationsRequests";
 import {getActivationRequestRoute} from "./routes/getAcitvationRequest";
 import {approveActivationRequestRoute} from "./routes/approveActivationRequest";
 import {declineActivationRequestRoute} from "./routes/declineActivationRequest";
+import cookieParser from "cookie-parser";
 
 
 
@@ -29,12 +30,14 @@ secure(app,{
     xss: true
 })
 
-app.use(cookieSession({
-    sameSite:"lax",
-    signed:false,
-    httpOnly:true, // to prevent xss and means it can never be accessed by JS
-    secure: false// note that even in production you will have to disable it because we are not https in prod yet
-}))
+// app.use(cookieSession({
+//     sameSite:"lax",
+//     signed:false,
+//     httpOnly:true, // to prevent xss and means it can never be accessed by JS
+//     secure: false// note that even in production you will have to disable it because we are not https in prod yet
+// }))
+
+app.use(cookieParser());
 
 
 app.use("/api/manage-business",submitActivationRequestRoute
