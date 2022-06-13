d39c711a79f482f3c9870be1a00503352a5346c1
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-06-13 21:44:05 +0200

    making an edit to password route

--- auth/src/routes/checkPassword.ts
+++ auth/src/routes/checkPassword.ts
@@ -1,6 +1,6 @@
 import express, {Request,Response} from "express";
 import {body, CustomValidator} from "express-validator";
-import {BadRequestError, validateRequest} from "@hashcash/common";
+import {BadRequestError, ErrorCodes, validateRequest} from "@hashcash/common";
 import {sendSuccess} from "@hashcash/common";
 import {generateFackUsers} from "../utils/generateFackUsers";
 import {User} from "../models/user";
@@ -15,7 +15,12 @@ router.post('/check-password',[
 ],validateRequest,async (req:Request,res:Response)=>{
   const {password} =req.body;
   if (password.length<11)
-      throw new BadRequestError(["Password must be more than or equal to 10 characters"],200);
+      return res.status(200).send({
+          status:"fail",
+          errorCode:ErrorCodes.badRequest,
+          errors: ["Password must be less than or equal to 10 characters"],
+
+      })
 
  sendSuccess(res,200,{});
 })
