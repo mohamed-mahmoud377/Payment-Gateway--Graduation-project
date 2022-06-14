ecb37d2f54d3c7e2d0c23887dc4090c8bbee4092
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-06-14 10:40:30 +0200

    updating how we generate otp

--- auth/src/utils/otpGenerator.ts
+++ auth/src/utils/otpGenerator.ts
@@ -1,3 +1,11 @@
 export const otpGenerator= ()=>{
    return Math.floor(Math.random() *100000)
+
+   while (true){
+      const  randomNumber=  Math.ceil(Math.random() *100000);
+      const randomNumberString = String(randomNumber);
+      if (randomNumberString.length===5){
+         return randomNumber;
+      }
+   }
 }
