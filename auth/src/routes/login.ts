40d73c142d3cc99abf9c6a2d0f9c4a74190be93a
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-06-19 19:41:58 +0200

    updateing auth

--- auth/src/routes/login.ts
+++ auth/src/routes/login.ts
@@ -77,8 +77,9 @@ router.post('/login',[
 
     // req.session= {jwt:accessToken};
     res.cookie("jwt",accessToken,{
-        // sameSite:"lax",
-        // httpOnly:true
+        sameSite:"none",
+        // httpOnly:true,
+
     })
 
     // if admin only make him logged in for 15 min do not give him the refresh token
