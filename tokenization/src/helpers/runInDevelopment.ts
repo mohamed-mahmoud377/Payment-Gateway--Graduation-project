8089dc4ce3d86dfcf4bbb5e7a2febec0bcbf5d0c
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-06-26 18:37:44 +0200

    make run in dev fun to run in prod because of all of the database restarts

--- tokenization/src/helpers/runInDevelopment.ts
+++ tokenization/src/helpers/runInDevelopment.ts
@@ -24,7 +24,7 @@ const generateServiceAccessToken =async ()=>{
 export const runInDevelopment =async (options={
     generateServiceAccessToken:true
 }) => {
-    if (process.env.NODE_ENV==='production'){
+    if (process.env.NODE_ENV==='development'||process.env.NODE_ENV==='production'){
         if (options.generateServiceAccessToken)
             await generateServiceAccessToken();
 
