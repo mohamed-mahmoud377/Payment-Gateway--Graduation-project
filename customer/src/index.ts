9e21ff5610601f227c3ec6cc6b036992f7d0d424
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-06-27 13:48:25 +0200

    getting everying ready to run checkout, customer and payment services in prod and dev

---
+++ customer/src/index.ts
@@ -0,0 +1,67 @@
+import {app} from './app'
+import mongoose from "mongoose";
+import {natsWrapper} from "./nats/nats-wrapper";
+import {EmailVerifiedListener} from "./events/listeners/emailVerifiedListener";
+import {MerchantActivationListener} from "./events/listeners/merchantActivationListener";
+
+
+
+
+const start = async ()=>{
+    console.log('Starting up customer service...')
+
+    if (!process.env.JWT_KEY){
+        throw new Error('JWT_KEY must be defined')
+    }
+    if (!process.env.MONGO_URI){
+        throw new Error("MONGO_URI must be defined !")
+    }
+    if (!process.env.NATS_CLIENT_ID){
+        throw new Error("NATS_CLIENT_ID must be defined !")
+    }
+    if (!process.env.NATS_URL){
+        throw new Error("NATS_URL must be defined !")
+    }
+    if (!process.env.NATS_CLUSTER_ID){
+        throw new Error("NATS_CLUSTER_ID must be defined !")
+    }
+    try{
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID,process.env.NATS_CLIENT_ID,process.env.NATS_URL)
+        natsWrapper.client.on('close',()=>{
+            console.log("NATS connection closed !")
+            process.exit();
+        })
+
+        // why are we doing this ?
+        // because when the program close NATS still try to reach it but we closed ! like on purpose
+        // so in the closing process we tell NATS that we want to close the connection immediately
+        // so we don't waste time trying  to reach it
+
+        process.on('SIGINT',()=>natsWrapper.client.close()); // this for when you close the program with ctrl z
+        process.on('SIGTERM',()=>natsWrapper.client.close()); // this when docker tries to kill the process gracefully
+
+
+        await mongoose.connect(process.env.MONGO_URI!);
+        console.log("connected to database successfully")
+
+        new EmailVerifiedListener(natsWrapper.client).listen();
+        new MerchantActivationListener(natsWrapper.client).listen();
+
+
+
+
+
+
+    }catch (e) {
+
+        console.log(e)
+    }
+
+    app.listen(3000,()=>{
+        console.log('customer srv is up and running on port 3000  ')
+    })
+}
+
+
+
+start();
