6dc8b13f7925a6833673ddcef96a783ad0e5c226
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-07-13 21:35:54 +0200

    get payment route

--- payment/src/routes/getPayment.ts
+++ payment/src/routes/getPayment.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-import {APIFilter, requireAuth, sendSuccess, validateRequest} from "@hashcash/common";
+import {APIFilter, ForbiddenError, NotFoundError, requireAuth, sendSuccess, validateRequest} from "@hashcash/common";
 import {Payment} from "../models/payment";
 import {query} from "express-validator";
 
@@ -7,6 +7,15 @@ const router = express.Router();
 
 
 router.get('/payments/:id',requireAuth(),async (req:Request,res:Response)=>{
+    const {id} = req.params;
+    const payment = await Payment.findById(id);
+    if (!payment)
+        throw new NotFoundError();
+
+    if (payment.merchantId!==req.currentUser?.id)
+        throw new ForbiddenError();
+
+    sendSuccess(res,200,{payment});
 
 })
 
