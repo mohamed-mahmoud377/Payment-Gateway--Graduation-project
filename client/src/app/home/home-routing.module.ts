bb80daff17b547ca7cf1c89577652f79fb2bd32f
salmayaser <salmayasser161@gmail.com>
2022-07-20 19:11:26 +0200

    Remove dublicated component

--- client/src/app/home/home-routing.module.ts
+++ client/src/app/home/home-routing.module.ts
@@ -6,7 +6,6 @@ import { PaymentsComponent } from './Components/payments/payments.component';
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivateAccountComponent } from './Components/activate-account/activate-account.component';
-import { PaymentsListComponent } from './Components/payments-list/payments-list.component';
 import { ProfileComponent } from './Components/profile/profile.component';
 import { HomeComponent } from './home.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
